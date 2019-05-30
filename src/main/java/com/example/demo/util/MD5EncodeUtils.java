b590581f563a3a390571b458d8c2327ff794eff4
mygithubyc <yc_learn@163.com>
2019-05-30 21:10:01 +0800

    commit md5 encoding java utils

---
+++ src/main/java/com/example/demo/util/MD5EncodeUtils.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package com.example.demo.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * File： MD5EncodeUtils.java<br>
+ * Title: <br>
+ * Description: 密码使用MD5编码方式加密<br>
+ * Company: <br>
+ * 
+ * @author
+ * @version
+ */
+public class MD5EncodeUtils {
+
+	/**
+	 * 
+	 * @param text
+	 * @return String <br>
+	 */
+	public static String encodeMD5Lower32Bit(String text) {
+
+		String result = null;
+
+		try {
+			MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+			byte[] byteText = text.getBytes();
+			messageDigest.digest(byteText);
+			byte[] byteResult = messageDigest.digest();
+
+			StringBuffer stringBuffer = new StringBuffer();
+			for (byte b : byteResult) {
+				int byteValue = b & 0xff;
+				if (byteValue < 16) {
+					stringBuffer.append(0);
+				}
+
+				stringBuffer.append(Integer.toHexString(byteValue));
+				result = stringBuffer.toString();
+			}
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	/**
+	 * 
+	 * @param text 加密字符串
+	 * @return String <br>
+	 */
+	public static String encodeMD5Upper32Bit(String text) {
+
+		String result = encodeMD5Lower32Bit(text);
+		return result.toUpperCase();
+	}
+}
